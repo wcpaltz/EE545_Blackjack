70b5124a2a8e224c4f4e425df6fc0281ca387686
wcpaltz <wcpaltz@uwm.edu>
2019-04-25 14:56:37 -0500

    Add files via upload
    weekend work

--- blackjack.c
+++ blackjack.c
@@ -1,5 +1,85 @@
-#include stdlib
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define CARDS 52
 
-main(){
-  // DO NOTHING
+int balance;
+int pot;
+int cardTypes[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+
+int bet();
+void play();
+void turn(int, int);
+int hit();
+void dealer();
+int getCard();
+int ace();
+
+int main(){
+  printf("Welcome to the FPGA BlackJack Simulator!\n");
+  srand(time(0));
+  play();
+  return 0;
+}
+
+void play(){
+  printf("Beginning game...\n500 credits added to your account\n");
+  balance = 500;
+  int card1 = 0;
+  int card2 = 0;
+  while(balance > 0){
+    card1 = getCard();
+    card2 = getCard();
+    turn(card1, card2);
+    balance = 0;
+  }
+}
+
+void turn(int mCard1, int mCard2){
+  char decision;
+  printf("Inside turn()...\n");
+  printf("Two cards: %i - %i\n", mCard1, mCard2);
+  printf("Current Score: %i\n", mCard1 + mCard2);
+  printf("Would you like to hit or stand (h/s)?\n");
+  scanf("%c\n", decision);
+  printf("my decision: %c", decision);
+}
+
+int getCard(){
+  printf("Inside getCard()...\n");
+  int card = (rand() % (12));
+  printf("Card dealt: %i\n", cardTypes[card]);
+  return cardTypes[card];
+}
+
+void dealer(){
+  printf("Inside dealer()...\n");
+  // TODO
+}
+
+int bet(){
+  printf("Inside bet()...\n");
+  printf("Current balance: %i\n", balance);
+  printf("Enter the amount you would like to bet.\n");
+  int myBet = 0;
+  scanf("%i", &myBet);
+  while(myBet >= balance){
+    printf("Your bet must be less than your current token balance.\n");
+    scanf("%i\n", &myBet);
+  }
+  balance -= myBet;
+  printf("You made a bet of %i. Good Luck!\n", myBet);
+  pot = myBet;
+  return 0;
+}
+
+int hit(){
+  printf("Inside hit()...\n");
+  return 0;
+  // TODO
+}
+
+int ace(){
+  printf("Inside ace()...\n");
+  return 0;
 }
